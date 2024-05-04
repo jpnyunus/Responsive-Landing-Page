6c36662c860a87185890d257ae07766039a63058
yunus <137651666+jpnyunus@users.noreply.github.com>
2024-05-04 11:19:54 +0300

    Update app.js
    adding comments

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Tema değiştirme düğmesini aliyoruz
     const themeToggleButton = document.getElementById('theme-toggle');
-    updateButtonText();
-
+    updateButtonText();  // Buton metnini güncelle
     themeToggleButton.addEventListener('click', () => {
-      const currentTheme = document.body.getAttribute('data-theme');
-      
+      const currentTheme = document.body.getAttribute('data-theme'); // Mevcut tema bilgisini al
+
+    // tema dark ise light yap, değilse 'dark' yap
       if (currentTheme === 'dark') {
         document.body.setAttribute('data-theme', 'light');
       } else {
@@ -14,9 +15,10 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   
     function updateButtonText() {
-    
+    // Mevcut tema bilgisini al
       const currentTheme = document.body.getAttribute('data-theme');
-      
+        
+      // Tema 'dark' ise toggle off ikonunu göster, değilse toggle on ikonunu göster
       if (currentTheme === 'dark') {
         themeToggleButton.innerHTML = '<i class="fa-solid fa-toggle-off fa-xl" style="color: #f2b7c2; cursor: pointer;"></i>';
       } else {
